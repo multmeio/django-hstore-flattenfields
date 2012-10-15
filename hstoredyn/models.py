09a98d356b7ceb52990bbf98e15a0acf28b5751b
Iuri Diniz <iuridiniz@gmail.com>
2012-10-14 23:48:06 -0300

    better Forms

--- hstoredyn/models.py
+++ hstoredyn/models.py
@@ -10,6 +10,7 @@ from django import forms
 from django_orm.postgresql import hstore
 from django.utils.datastructures import SortedDict
 import copy
+from django.forms.models import fields_for_model
 
 class DynamicFields(models.Model):
     refer = models.CharField(max_length=120, blank=False)
@@ -119,71 +120,6 @@ class MyModel(models.Model):
         super(MyModel, self).__init__(*args, **kwargs)
 
 
-#######################################################
-def dynfields_for_model(model, fields=None, exclude=None, widgets=None, formfield_callback=None):
-    """
-    based on django fields_for_model
-     
-    Returns a ``SortedDict`` containing form fields for the given model.
-
-    ``fields`` is an optional list of field names. If provided, only the named
-    fields will be included in the returned fields.
-
-    ``exclude`` is an optional list of field names. If provided, the named
-    fields will be excluded from the returned fields, even if they are listed
-    in the ``fields`` argument.
-    """
-    field_list = []
-    ignored = []
-    refer = model.__name__
-
-    for metafield in DynamicFields.objects.filter(refer=refer):
-        type_ = metafield.type
-        try:
-            field_klass = eval('models.%s' % type_)
-            kwargs = dict()
-            f = field_klass(name=metafield.name,
-                            max_length=metafield.max_length,
-                            choices=metafield.choices.get('choices'),
-                            blank=metafield.blank,
-                            null=metafield.null)
-        except:
-            raise TypeError('Cannot create field for %r, maybe type %r ' + \
-                            'is not a django type' % (metafield, type_))
-
-        if not f.editable:
-            continue
-        if fields is not None and not f.name in fields:
-            continue
-        if exclude and f.name in exclude:
-            continue
-        if widgets and f.name in widgets:
-            kwargs = {'widget': widgets[f.name]}
-        else:
-            kwargs = {}
-
-        if formfield_callback is None:
-            formfield = f.formfield(**kwargs)
-        elif not callable(formfield_callback):
-            raise TypeError('formfield_callback must be a function or callable')
-        else:
-            formfield = formfield_callback(f, **kwargs)
-
-        formfield._model_field = f
-
-        if formfield:
-            field_list.append((f.name, formfield))
-        else:
-            ignored.append(f.name)
-    field_dict = SortedDict(field_list)
-    if fields:
-        field_dict = SortedDict(
-            [(f, field_dict.get(f)) for f in fields
-                if ((not exclude) or (exclude and f not in exclude)) and (f not in ignored)]
-        )
-    return field_dict
-######################################
-
 class MyModelFormMeta(forms.ModelForm.__metaclass__):
     def __new__(cls, name, bases, attrs):
         super_new = super(MyModelFormMeta, cls).__new__
@@ -201,20 +137,17 @@ class MyModelFormMeta(forms.ModelForm.__metaclass__):
 class MyModelForm(forms.ModelForm):
     __metaclass__ = MyModelFormMeta
     def __init__(self, *args, **kwargs):
-        # add dynamic fields to form
+        super(MyModelForm, self).__init__(*args, **kwargs)
+        # Always override for fields (dynamic fields maybe deleted/included) 
         opts = self._meta
-        if opts.model:
+        if opts.model and issubclass(opts.model, MyModel):
             # If a model is defined, extract dynamic form fields from it.
             if not opts.exclude:
                 opts.exclude = []
             # hide dfields
             opts.exclude.append('_dfields')
-            dynamic_fields = dynfields_for_model(opts.model, opts.fields,
-                                      opts.exclude, opts.widgets)
-            self.base_fields = copy.deepcopy(self.base_fields)
-            self.base_fields.update(dynamic_fields)
-
-        super(MyModelForm, self).__init__(*args, **kwargs)
+            self.fields = fields_for_model(opts.model, opts.fields,
+                                              opts.exclude, opts.widgets)
 
 
 #######################################################
