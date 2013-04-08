f594d1d91a345c73550b7f370d0e1cb93ef447a3
Luan fonseca de farias <luanfonceca@gmail.com>
2013-04-08 15:07:39 -0300

    Fixing the Views file

--- example/hstoredyn/views.py
+++ example/hstoredyn/views.py
@@ -4,36 +4,33 @@ Created on 13/10/2012
 @author: iuri
 '''
 
-from django.template import Context, loader
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 from hstoredyn.models import Something
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
-from hstoredyn.models import SomethingForm
+
+from models import SomethingForm
 
 def index(request):
-    somethings = Something.objects.all()
-    t = loader.get_template('something/index.html')
-    c = Context({
-        'somethings_list': somethings,
-    })
-    return HttpResponse(t.render(c))
+    data = {
+        'something': Something.objects.all()
+    }
+    return render_to_response('something/index.html', data,
+        context_instance=RequestContext(request))
 
 def detail(request, some_id):
     return HttpResponse("You're looking at something %s." % some_id)
 
 def add(request):
-    if request.method == 'POST': # If the form has been submitted
-        form = SomethingForm(request.POST) # A form bound to the POST data
-        if form.is_valid(): # All validation rules pass
-            # Process the data in form.cleaned_data
-            # ...
-            form.save()
-            return HttpResponseRedirect('/thanks/') # Redirect after POST
-    else:
-        form = SomethingForm() # An unbound form
-        #form.as_p()
+    form = SomethingForm(request.POST or None) # A form bound to the POST data
 
-    return render(request, 'something/add.html', {
-        'form': form,
-    })
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect('/thanks/') # Redirect after POST
+
+    data = {
+        'form': form
+    }
+    return render_to_response('something/add.html', data,
+        context_instance=RequestContext(request))
