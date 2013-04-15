924d06938c20423478a89f60099d5dc51fd248cc
Luan fonseca de farias <luanfonceca@gmail.com>
2013-04-15 11:23:59 -0300

    Adding the DynamicField Model in admin file

--- example/hstoredyn/admin.py
+++ example/hstoredyn/admin.py
@@ -3,7 +3,10 @@ Created on 13/10/2012
 
 @author: iuri
 '''
-from hstoredyn.models import Something
+from hstore_flattenfields.models import DynamicField
 from django.contrib import admin
 
+from hstoredyn.models import Something
+
 admin.site.register(Something)
+admin.site.register(DynamicField)
