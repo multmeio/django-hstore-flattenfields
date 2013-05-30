156afa315e1dbbe51174d905e50e87dc91e3ff52
Luan fonseca de farias <luanfonceca@gmail.com>
2013-05-30 16:58:09 -0300

    Adding the initial portability of all the Django query Filters tests, creating a new project just for the tests and adding the data type parse of the DynamicField in level of object.

--- hstore_flattenfields/utils.py
+++ hstore_flattenfields/utils.py
@@ -10,12 +10,16 @@ Copyright (c) 2012 Multmeio [design+tecnologia]. All rights reserved.
 from django.template.defaultfilters import slugify, floatformat
 from django.db import connection
 from ast import literal_eval
+from datetime import datetime
 
 __all__ = ['single_list_to_tuple',
            'str2literal',
            'dec2real',
            'has_any_in',
-           'dynamic_field_table_exists'
+           'dynamic_field_table_exists',
+           'str2date',
+           'str2datetime',
+           'force_create_table',
 ]
 
 
@@ -31,6 +35,20 @@ def str2literal(string):
 def dec2real(value):
     return floatformat(value, 2)
 
+def str2datetime(value, format="%Y-%m-%d %H:%M:%S.%f"):
+    try:
+        return datetime.strptime(
+            value, format
+        )
+    except:
+        return ''
+
+def str2date(value):
+    try:
+        return str2datetime(value, "%Y-%m-%d").date()
+    except:
+        return ''
+
 def has_any_in(chances, possibilities):
     return any([x for x in chances if x in possibilities])
 
@@ -39,8 +57,75 @@ _DYNAMIC_FIELD_TABLE_EXISTS = None
 def dynamic_field_table_exists():
     # NOTE: Error happen on syncdb, because DynamicField's table does not exist.
     global _DYNAMIC_FIELD_TABLE_EXISTS
-    if _DYNAMIC_FIELD_TABLE_EXISTS == None:
-        cursor = connection.cursor()
-        cursor.execute("select count(*) from pg_tables where tablename='dynamic_field'")
-        _DYNAMIC_FIELD_TABLE_EXISTS = cursor.fetchone()[0] > 0
+    _DYNAMIC_FIELD_TABLE_EXISTS = 'dynamic_field' in connection.introspection.table_names()
+    # if _DYNAMIC_FIELD_TABLE_EXISTS == None:
+    #     cursor = connection.cursor()
+    #     cursor.execute("select count(*) from pg_tables where tablename='dynamic_field'")
+    #     _DYNAMIC_FIELD_TABLE_EXISTS = cursor.fetchone()[0] > 0
     return _DYNAMIC_FIELD_TABLE_EXISTS
+
+
+def force_create_table():
+    cursor = connection.cursor()
+    cursor.execute("""
+        -- Table: dynamic_field
+
+        -- DROP TABLE dynamic_field;
+
+        CREATE TABLE dynamic_field
+        (
+          id serial NOT NULL,
+          refer character varying(120) NOT NULL,
+          name character varying(120) NOT NULL,
+          verbose_name character varying(120) NOT NULL,
+          typo character varying(20) NOT NULL,
+          max_length integer,
+          "order" integer,
+          blank boolean NOT NULL,
+          choices text,
+          default_value character varying(80),
+          CONSTRAINT dynamic_field_pkey PRIMARY KEY (id),
+          CONSTRAINT dynamic_field_name_key UNIQUE (name)
+        )
+        WITH (
+          OIDS=FALSE
+        );
+        ALTER TABLE dynamic_field
+          OWNER TO zambe;
+
+        -- Index: dynamic_field_refer
+
+        -- DROP INDEX dynamic_field_refer;
+
+        CREATE INDEX dynamic_field_refer
+          ON dynamic_field
+          USING btree
+          (refer COLLATE pg_catalog."default");
+
+        -- Index: dynamic_field_refer_like
+
+        -- DROP INDEX dynamic_field_refer_like;
+
+        CREATE INDEX dynamic_field_refer_like
+          ON dynamic_field
+          USING btree
+          (refer COLLATE pg_catalog."default" varchar_pattern_ops);
+
+        -- Index: dynamic_field_typo
+
+        -- DROP INDEX dynamic_field_typo;
+
+        CREATE INDEX dynamic_field_typo
+          ON dynamic_field
+          USING btree
+          (typo COLLATE pg_catalog."default");
+
+        -- Index: dynamic_field_typo_like
+
+        -- DROP INDEX dynamic_field_typo_like;
+
+        CREATE INDEX dynamic_field_typo_like
+          ON dynamic_field
+          USING btree
+          (typo COLLATE pg_catalog."default" varchar_pattern_ops);
+    """)
