5318b4a9ef2eef144d4fb5b41be3ebae6a3defd2
Diego Toral <diegotoral@gmail.com>
2013-09-12 11:01:11 -0300

    Remove as_tabs method and add an as_tabs filter

---
+++ hstore_flattenfields/templatetags/content_panes.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from django import template
+from django.template import Context, loader
+
+from hstore_flattenfields.forms import HStoreContentPaneModelForm
+
+register = template.Library()
+
+
+@register.filter(name='as_tabs')
+def as_tabs(form):
+    """
+    Shows the form fields grouped in tabs.
+    """
+    if not isinstance(form, HStoreContentPaneModelForm):
+        return ''
+
+    context = Context({ 'form': form })
+    template = 'hstore_flattenfields/form.html'
+
+    return loader.get_template(template).render(context)
