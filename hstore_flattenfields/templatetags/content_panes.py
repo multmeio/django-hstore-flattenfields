c379acb1fff9f54704c074a2bd7b8fe76ae64cb4
Luan fonseca de farias <luanfonceca@gmail.com>
2013-10-18 10:27:19 -0300

    Reorganizing the template dirs, in order to allow the user to will have all the rights to built his own layout or they just copy our template folder and remove the .sample sufix.

--- hstore_flattenfields/templatetags/content_panes.py
+++ hstore_flattenfields/templatetags/content_panes.py
@@ -9,14 +9,40 @@ from hstore_flattenfields.forms import HStoreContentPaneModelForm
 register = template.Library()
 
 
+def generic_loader(form, context, template):
+    if not isinstance(form, HStoreContentPaneModelForm):
+        return ''
+    return loader.get_template(template).render(Context(context))
+
 @register.filter(name='as_tabs')
-def as_tabs(form, template='hstore_flattenfields/form.html'):
+def as_tabs(form):
     """
     Shows the form fields grouped in tabs.
     """
-    if not isinstance(form, HStoreContentPaneModelForm):
-        return ''
+    return generic_loader(
+        form=form, 
+        context={'form': form}, 
+        template='hstore_flattenfields/form.html'
+    )
 
-    context = Context({'form': form})
+@register.filter(name='render_form_without_tabs')
+def render_form_without_tabs(form):
+    """
+    Shows only the form without any tab.
+    """
+    return generic_loader(
+        form=form, 
+        context={'form': form}, 
+        template='hstore_flattenfields/form_without_tabs.html'
+    )
 
-    return loader.get_template(template).render(context)
+@register.filter(name='render_form_tabs')
+def render_form_tabs(form):
+    """
+    Shows only the tabs.
+    """
+    return generic_loader(
+        form=form, 
+        context={'form': form}, 
+        template='hstore_flattenfields/form_tabs.html'
+    )
